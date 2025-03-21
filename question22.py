edefe8186f46c194ffc3ede25d09e1de8e9f2cd1
Shehneen Fatima <shehneenfatima@gmail.com>
2025-03-21 17:35:05 +0500

    Added solution of question22

---
+++ question22.py
@@ -0,0 +1,7 @@
+
+#Write a program that calculates and prints the sum of the even integers from 2 to 30.
+def sum_of_even_numbers():
+    total = sum(range(2, 31, 2))  # Sum of even numbers from 2 to 30
+    print("The sum of even integers from 2 to 30 is:", total)
+
+sum_of_even_numbers()
